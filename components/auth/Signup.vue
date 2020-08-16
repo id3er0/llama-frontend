bebab5a797817e0678ac097380b580a5546b9be9
ID 0 <id0@posteo.net>
2020-08-16 08:35:27 +0300

    Add signup.

---
+++ components/auth/Signup.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+  .sisu-page
+    .form
+      .form-error(
+        v-for="(error, index) in errors"
+        :key="index"
+        v-if="error"
+      ) {{error.message}}
+      .form-group
+        label.label(for="name") Name
+        input#name.input(
+          :class="{error: nameError}"
+          v-model="name"
+          autofocus
+        )
+      .form-group
+        label.label(for="email") Email
+        input#email.input(
+          :class="{error: emailError}"
+          v-model="email"
+        )
+      .form-group
+        label.label(for="password") Password
+        input#password.input(
+          :class="{error: passwordError}"
+          v-model="password"
+          type="password"
+        )
+      .form-buttons.buttons
+        button.button-wrapper
+          span.button.button-primary.disabled(
+            tabindex="-1"
+            @click="createUser"
+          ) Sign Up
+        button.button-wrapper
+          nuxt-link.button.button-secondary(
+            tabindex="-1"
+            :to="{name: 'login'}"
+          ) Log In
+</template>
+
+
+<script>
+  import { mapFields } from 'vuex-map-fields';
+  import { mapActions } from 'vuex';
+
+  export default {
+    name: 'Signup',
+    async created() {
+      console.log('xxx:', await this.$fireAuth.currentUser);
+    },
+    computed: {
+      ...mapFields('signup', [
+        'payload.name',
+        'payload.email',
+        'payload.password',
+        'errors',
+        'errors.nameError',
+        'errors.emailError',
+        'errors.passwordError',
+      ]),
+    },
+    methods: {
+      ...mapActions('signup', [
+        'createUser',
+      ]),
+    },
+    watch: {
+      name() {
+        this.nameError = null;
+      },
+      email() {
+        this.emailError = null;
+      },
+      password() {
+        this.passwordError = null;
+      },
+    },
+  };
+</script>
---
+++ store/signup.js
@@ -0,0 +1,65 @@
+import { Map as immutableMap } from 'immutable';
+import { getField, updateField } from 'vuex-map-fields';
+// import { debounce, throttle } from 'throttle-debounce';
+// import ky from 'ky';
+
+const STATE = immutableMap({
+  date: Date.now(),
+  authorized: false,
+  payload: {
+    name: null,
+    email: null,
+    password: null,
+  },
+  errors: {
+    nameError: null,
+    emailError: null,
+    passwordError: null,
+  },
+});
+
+export const state = () => STATE.toJS();
+
+export const getters = {
+  getField,
+};
+
+export const mutations = {
+  updateField,
+  clean(state) {
+    Object.assign(state, {
+      ...STATE.toJS(),
+      date: Date.now(),
+    });
+  },
+};
+
+export const actions = {
+  async createUser(context) {
+    context.commit('updateField', {path: 'errors', value: STATE.toJS().errors});
+    try {
+      await this.$fireAuth.createUserWithEmailAndPassword(
+        context.state.payload.email,
+        context.state.payload.password,
+      );
+      const user = await this.$fireAuth.currentUser;
+      await user.updateProfile({
+        displayName: context.state.payload.name,
+      });
+    } catch (error) {
+      console.warn('xxx createUser - error:', error);
+
+      switch (error.code) {
+        case 'auth/weak-password':
+          context.commit('updateField', {path: 'errors.passwordError', value: {message: error.message}});
+          break;
+        case 'auth/email-already-in-use':
+          context.commit('updateField', {path: 'errors.emailError', value: {message: error.message}});
+          break;
+        default:
+          context.commit('updateField', {path: 'errors.emailError', value: {message: error.message}});
+          break;
+      }
+    }
+  },
+};
